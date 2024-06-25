1d7e9913fc61a3fdfdb62406b88c4faaabc7f19a
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2024-06-25 16:45:46 -0400

    Linting

--- cases/run_cases.py
+++ cases/run_cases.py
@@ -7,16 +7,20 @@ import shutil
 # A script to generate input files and run F-layer cases
 # NB: this runs in serial and takes about a week to finish.
 
-# Lists of parameters - by default, I suppose we could take these from command line
+# Lists of parameters - by default, I suppose we could take these from
+# command line
 i0 = [1.0E-7, 1.0E-8, 1.0E-9, 1.0E-10, 1.0E-11]
-dl_mu_pairs = [(1.0E-11, 2.0E-6), (1.0E-11, 3.0E-5), (1.0E-11, 6.0E-8), (1.0E-12, 2.0E-6), (1.0E-9, 2.0E-6)]
+dl_mu_pairs = [(1.0E-11, 2.0E-6), (1.0E-11, 3.0E-5), (1.0E-11, 6.0E-8), 
+               (1.0E-12, 2.0E-6), (1.0E-9, 2.0E-6)]
 
 # Template
 template_path = pathlib.Path("template")
 
+
 def dir_name(myi0, dl, mu):
     name = f"i0_{myi0:1.0E}_dl_{dl:1.0E}_mu_{mu:1.0E}"
     return name
+
 
 def replace_params(infile, outfile, i0, dl, mu):
     """
@@ -27,8 +31,10 @@ def replace_params(infile, outfile, i0, dl, mu):
 
     dat = dat.replace('# This is the "Light blue square" from the paper\n', '')
     dat = dat.replace("i0 : 1.0e-11", f"i0 : {i0:E}")
-    dat = dat.replace("chemical_diffusivity : 1.0e-9", f"chemical_diffusivity : {dl:E}")
-    dat = dat.replace("kinematic_viscosity : 2.0e-6", f"kinematic_viscosity : {mu:E}")
+    dat = dat.replace("chemical_diffusivity : 1.0e-9", 
+                      f"chemical_diffusivity : {dl:E}")
+    dat = dat.replace("kinematic_viscosity : 2.0e-6", 
+                      f"kinematic_viscosity : {mu:E}")
 
     with open(outfile, 'w+') as outfh:
         outfh.write(dat)
@@ -43,7 +49,8 @@ def main(i0=i0, dl_mu_pairs=dl_mu_pairs):
             print(f"Working on {target_path}")
             if not target_path.exists():
                 target_path = shutil.copytree(template_path, target_path)
-                replace_params(target_path/"parameters.yaml", target_path/"parameters.yaml", myi0, dl, mu)
+                replace_params(target_path/"parameters.yaml", 
+                               target_path/"parameters.yaml", myi0, dl, mu)
                 print(" -> files ready, about to run")
                 subprocess.run(["./run.sh"], cwd=target_path.resolve())
                 print(" -> done")
